8eb5313fcbca1ad8097a95da3ab6a0729463aceb
Adrian Zuber <xadrianzetx@gmail.com>
2021-01-11 20:38:01 +0100

    wow

--- aoc/d5.py
+++ aoc/d5.py
@@ -1,8 +1,11 @@
+import re
 import numpy as np
 
 
 def decode_pass(code: str) -> int:
     """
+    As a sanity check, look through your list of boarding passes.
+    What is the highest seat ID on a boarding pass?
     """
 
     rows = np.arange(1, 129)
@@ -26,9 +29,21 @@ def decode_pass(code: str) -> int:
 
 def find_seat(ids: list) -> int:
     """
+    What is the ID of your seat?
     """
 
     rng = np.arange(min(ids), max(ids) + 1)
     for i in rng:
         if i not in ids and i - 1 in ids and i + 1 in ids:
             return i
+
+
+def decode_pass_genius_way(code: str) -> int:
+    """
+    wow
+    https://github.com/fuglede/adventofcode/blob/master/2020/day05/solutions.py
+    """
+
+    code = re.sub('[FL]', '0', code)
+    code = re.sub('[BR]', '1', code)
+    return int(code, base=2)
