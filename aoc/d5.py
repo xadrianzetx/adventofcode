fae0e75bbeb4094a6d2c8ab511d8029ade63ef78
Adrian Zuber <xadrianzetx@gmail.com>
2021-01-11 20:33:41 +0100

    part two

--- aoc/d5.py
+++ aoc/d5.py
@@ -1,7 +1,10 @@
 import numpy as np
 
 
-def find_seat(code: str) -> int:
+def decode_pass(code: str) -> int:
+    """
+    """
+
     rows = np.arange(1, 129)
     cols = np.arange(1, 9)
     for chr in code:
@@ -19,3 +22,13 @@ def find_seat(code: str) -> int:
             cols = cols[cols > np.mean(cols)]
     seat_id = ((rows[0] - 1) * 8) + (cols[0] - 1)
     return seat_id
+
+
+def find_seat(ids: list) -> int:
+    """
+    """
+
+    rng = np.arange(min(ids), max(ids) + 1)
+    for i in rng:
+        if i not in ids and i - 1 in ids and i + 1 in ids:
+            return i
