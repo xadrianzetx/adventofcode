ef5ec97761b274cd0323357c172a1107af0fa41a
Adrian Zuber <xadrianzetx@gmail.com>
2021-01-12 19:40:18 +0100

    part two

--- aoc/d6.py
+++ aoc/d6.py
@@ -21,3 +21,28 @@ def declaration_form_counter(path: str) -> list:
                 form = ''
 
     return np.sum(forms)
+
+
+def strict_declaration_form_counter(path: str) -> list:
+    """
+    For each group, count the number of questions
+    to which everyone answered "yes". What is the sum of those counts?
+    """
+
+    forms = []
+    with open(path, 'r') as file:
+        form = ''
+        pcount = 0
+        for line in file:
+            if line != '\n':
+                form += line.strip()
+                pcount += 1
+            else:
+                chrs = list(form)
+                uchrs, chrcounts = np.unique(chrs, return_counts=True)
+                qcounts = len(uchrs[chrcounts == pcount])
+                forms.append(qcounts)
+                form = ''
+                pcount = 0
+
+    return np.sum(forms)
