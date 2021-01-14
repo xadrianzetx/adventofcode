6fb2f9fc6e154d24c2df4d55f42b141916936d7d
Adrian Zuber <xadrianzetx@gmail.com>
2021-01-14 19:43:46 +0100

    part two

--- aoc/d8.py
+++ aoc/d8.py
@@ -1,14 +1,23 @@
+import copy
+
+
 def interpreter(program: list) -> int:
+    """
+    Part one
+    """
+
     acc = 0
     last_acc = 0
     pcounter = 0
     executed = [0]
+    exit_code = 1
 
     while True:
         try:
             op, arg = program[pcounter].split()
             arg = int(arg)
         except IndexError:
+            exit_code = 0
             break
 
         if op == 'nop':
@@ -27,4 +36,31 @@ def interpreter(program: list) -> int:
         last_acc = acc
         executed.append(pcounter)
 
-    return last_acc
+    return last_acc, exit_code
+
+
+def self_fixing_interpreter(program: str) -> int:
+    """
+    Part two
+    """
+
+    for i in range(len(program)):
+        tested_op, arg = program[i].split()
+        pcopy = copy.deepcopy(program)
+
+        if tested_op == 'nop':
+            pcopy[i] = f'jmp {arg}'
+
+        elif tested_op == 'jmp':
+            pcopy[i] = f'nop {arg}'
+
+        else:
+            # no reason to test program on acc
+            continue
+
+        acc, exit_code = interpreter(pcopy)
+
+        if exit_code == 0:
+            break
+
+    return acc
