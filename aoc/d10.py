6e8abb3458769c876e55af03f3f9d5d6c32108b4
Adrian Zuber <xadrianzetx@gmail.com>
2021-01-16 16:43:51 +0100

    part two

--- aoc/d10.py
+++ aoc/d10.py
@@ -17,3 +17,26 @@ def chain_adapters(adapters: np.array) -> int:
     _, jcounts = np.unique(jdiffs, return_counts=True)
 
     return np.product(jcounts), jdiffs
+
+
+def combine_adapters(arr: np.array) -> int:
+    """
+    max length of consecutive nums is 5
+
+    lengths of 3 have 2 combos
+    lengths of 4 have 2^2=4 combos
+    lengths of 5 have 2^3-1=7 combos
+    """
+
+    data = np.sort(arr)
+    data = np.insert(data, 0, 0)
+    data = np.append(data, max(data) + 3)
+    seq = np.split(data, np.where(np.diff(data) != 1)[0] + 1)
+    seq_len = np.array([len(s) for s in seq])
+
+    a = 7 ** len(seq_len[seq_len == 5])
+    b = 4 ** (len(seq_len[seq_len == 4]))
+    c = 2 * len(seq_len[seq_len == 3])
+    combos = a * b * c
+
+    return combos
