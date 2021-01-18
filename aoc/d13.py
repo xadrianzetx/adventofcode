e41041760efaee77c1e789492a4f13e1ccc8c613
Adrian Zuber <xadrianzetx@gmail.com>
2021-01-18 20:01:33 +0100

    part two

--- aoc/d13.py
+++ aoc/d13.py
@@ -1,4 +1,5 @@
 import numpy as np
+from functools import reduce
 
 
 def pick_bus(depart: int, schedule: str) -> int:
@@ -11,3 +12,40 @@ def pick_bus(depart: int, schedule: str) -> int:
     running = [int(b) for b in schedule.split(',') if b != 'x']
     delays = [b - depart % b for b in running]
     return running[np.argmin(delays)] * np.min(delays)
+
+
+def chinese_remainder(n: np.array, a: np.array) -> int:
+    sum = 0
+    prod = reduce(lambda a, b: a * b, n)
+    for n_i, a_i in zip(n, a):
+        p = prod // n_i
+        sum += a_i * mul_inv(p, n_i) * p
+    return sum % prod
+
+
+def mul_inv(a: int, b: int) -> int:
+    b0 = b
+    x0, x1 = 0, 1
+    if b == 1:
+        return 1
+    while a > 1:
+        q = a // b
+        a, b = b, a % b
+        x0, x1 = x1 - q * x0, x0
+    if x1 < 0:
+        x1 += b0
+    return x1
+
+
+def earliest_timestamp(schedule: str) -> int:
+    """
+    https://rosettacode.org/wiki/Chinese_remainder_theorem
+    """
+
+    schedule = schedule.split(',')
+    offsets = np.arange(len(schedule))
+    offsets = np.array([o for o, b in zip(offsets, schedule) if b != 'x']).astype('int64')
+    n = np.array([int(b) for b in schedule if b != 'x']).astype('int64')
+    a = n - offsets
+    res = chinese_remainder(n, a)
+    return res
