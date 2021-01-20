4a33bb2ee0629344a930e581b983d4c5906b0547
Adrian Zuber <xadrianzetx@gmail.com>
2021-01-20 09:11:42 +0100

    part two

--- aoc/d14.py
+++ aoc/d14.py
@@ -1,4 +1,5 @@
 import re
+import itertools
 import numpy as np
 
 
@@ -11,7 +12,7 @@ def emulate_decoder(program: list) -> int:
     """
 
     mask = None
-    mem = np.zeros((128000, )).astype('int64')
+    mem = {}
 
     for op in program:
         if op[:3] == 'mem':
@@ -33,4 +34,41 @@ def emulate_decoder(program: list) -> int:
         else:
             mask = re.findall('= (.*)', op)[0]
 
-    return sum(mem)
+    return sum(mem.values())
+
+
+def memory_addres_decoder(program: list) -> int:
+    """
+    Execute the initialization program
+    using an emulator for a version 2 decoder chip.
+    What is the sum of all values left in memory after it completes?
+    """
+
+    mask = None
+    mem = {}
+
+    for op in program:
+        if op[:3] == 'mem':
+            value = int(re.findall('= (.*)', op)[0])
+            addr = int(re.findall('\[(.*?)\]', op)[0])
+
+            addr = bin(addr)[2:]
+            addr = list(addr.zfill(len(mask)))
+
+            for pos, val in enumerate(mask):
+                if val in ['X', '1']:
+                    addr[pos] = val
+
+            addr = np.array(addr)
+            flt = addr[addr == 'X']
+
+            for prd in itertools.product([0, 1], repeat=flt.shape[0]):
+                tmp = np.array(addr, copy=True)
+                tmp[tmp == 'X'] = prd
+                memaddr = int(''.join(tmp), 2)
+                mem[memaddr] = value
+
+        else:
+            mask = re.findall('= (.*)', op)[0]
+
+    return sum(mem.values())
