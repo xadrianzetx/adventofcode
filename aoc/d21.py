fd72a20ef34edf643d26c75c2b73367d021ca54d
Adrian Zuber <xadrianzetx@gmail.com>
2021-01-29 09:53:03 +0100

    part two

--- aoc/d21.py
+++ aoc/d21.py
@@ -35,7 +35,7 @@ def build_ingredients_list(foods: list) -> dict:
     return alg_dict, ing_dict
 
 
-def get_safe_foods(allergens: dict, ingredients: dict) -> int:
+def get_safe_foods(allergens: dict, ingredients: dict) -> tuple:
     """
     Determine which ingredients cannot
     possibly contain any of the allergens in your list.
@@ -53,4 +53,44 @@ def get_safe_foods(allergens: dict, ingredients: dict) -> int:
     safe = {k: v for k, v in ingredients.items() if k not in unsafe}
     count = sum(list(safe.values()))
 
-    return count
+    return count, safe
+
+
+def get_unsafe_foods(allergens: dict, safe: list) -> str:
+    """
+    Find canonical list of unsafe ingredients
+    """
+
+    unsafe = {}
+    candidates = {}
+
+    for alg, attr in allergens.items():
+        # filter ingredients list from confirmed safe
+        x = [i for i in attr['ing'] if i not in safe.keys()]
+        ing, counts = np.unique(x, return_counts=True)
+        ing = ing[counts == attr['count']]
+        if alg not in candidates.keys():
+            candidates[alg] = []
+        candidates[alg] = ing
+
+    while True:
+        # get one to one
+        cands = [k for k, v in candidates.items() if len(v) == 1]
+
+        if len(cands) == 0:
+            # finished
+            break
+
+        for cand in cands:
+            # match confirmed par
+            ing = candidates[cand][0]
+            unsafe[ing] = cand
+            for alg in candidates.keys():
+                # pop matched
+                candidates[alg] = [a for a in candidates[alg] if a != ing]
+
+    sitems = sorted(unsafe.items(), key=lambda item: item[1])
+    sitems = [item[0] for item in sitems]
+    canon = ','.join(sitems)
+
+    return canon
