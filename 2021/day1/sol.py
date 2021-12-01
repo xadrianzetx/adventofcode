834923a75147deccd50d5bb34ba72ded4bb35cd2
xadrianzetx <xadrianzetx@gmail.com>
2021-12-01 12:55:33 +0100

    Fix naming

--- 2021/day1/sol.py
+++ 2021/day1/sol.py
@@ -8,7 +8,7 @@ def read_sweep_report(filename: str) -> np.array:
     return np.array(report, dtype=np.int32)
 
 
-def count_increases(report: np.array) -> int:
+def count_increasing(report: np.array) -> int:
 
     diffs = report[1:] - report[:-1]
     return len(diffs[diffs > 0])
@@ -20,10 +20,9 @@ def three_measurements(report: np.array) -> np.array:
 
 
 if __name__ == "__main__":
-
     report = read_sweep_report("d1.txt")
-    part1 = count_increases(report)
+    part1 = count_increasing(report)
     print(part1)
 
-    part2 = count_increases(three_measurements(report))
+    part2 = count_increasing(three_measurements(report))
     print(part2)
