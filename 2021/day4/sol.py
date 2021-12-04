bb06952345f72672387855fbdcb8358fb2cc8b45
xadrianzetx <xadrianzetx@gmail.com>
2021-12-04 17:17:58 +0100

    Day 4 part 2

--- 2021/day4/sol.py
+++ 2021/day4/sol.py
@@ -9,6 +9,7 @@ class BingoBoard:
         self._numbers = np.array(numbers)
         self._hits = np.zeros((5, 5), dtype=np.uint8)
         self._last_called = 0
+        self._finished = False
 
     def update(self, number: int) -> None:
 
@@ -17,11 +18,17 @@ class BingoBoard:
         if len(hit) > 0:
             self._hits[hit[0][0], hit[0][1]] = 1
 
+    def has_finished(self) -> bool:
+        return self._finished
+
     def has_bingo(self) -> bool:
 
         row_bingo = 5 in np.sum(self._hits, axis=0, dtype=np.uint8)
         col_bingo = 5 in np.sum(self._hits, axis=1, dtype=np.uint8)
-        return row_bingo or col_bingo
+        bingo = row_bingo or col_bingo
+        self._finished = bingo
+
+        return bingo
 
     def get_score(self) -> int:
 
@@ -61,16 +68,29 @@ def read_bingo(filename: str) -> Tuple[List[int], List[BingoBoard]]:
     return numbers, boards
 
 
-def play_bingo(numbers: List[int], boards: List[BingoBoard]) -> int:
+def play_bingo(numbers: List[int], boards: List[BingoBoard]) -> Tuple[int, int]:
+
+    boards_left = len(boards)
+    first_win = None
 
     for number in numbers:
         for board in boards:
             board.update(number)
-            if board.has_bingo():
-                return board.get_score()
+
+            if not board.has_finished() and board.has_bingo():
+                if boards_left != 1:
+                    boards_left -= 1
+                    if first_win is None:
+                        first_win = board.get_score()
+
+                else:
+                    last_win = board.get_score()
+                    return first_win, last_win
 
 
 if __name__ == "__main__":
     numbers, boards = read_bingo("d4.txt")
-    part1 = play_bingo(numbers, boards)
+
+    part1, part2 = play_bingo(numbers, boards)
     print(part1)
+    print(part2)
