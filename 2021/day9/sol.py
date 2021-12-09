6a438f91ec359164496758d4107c088b6fcc35f0
xadrianzetx <xadrianzetx@gmail.com>
2021-12-09 12:55:58 +0100

    Day 9 part 2

--- 2021/day9/sol.py
+++ 2021/day9/sol.py
@@ -1,39 +1,42 @@
-from typing import Dict
+import numpy as np
+from scipy.ndimage import label
+from scipy.ndimage.filters import generic_filter
 
-ADJACENT = [1 + 0j, -1 + 0j, 0 + 1j, 0 - 1j]
 
+def make_height_map(filename: str) -> np.array:
 
-def make_height_map(filename: str) -> Dict[complex, int]:
-
-    location = 0 + 0j
-    height_map = {}
+    data = []
     with open(filename) as file:
         for line in file:
             heights = list(line.rstrip())
-            for h in heights:
-                height_map[location] = int(h)
-                location += 1
-            location += -location.real + 1j
+            data.append(heights)
 
-    return height_map
+    return np.array(data, dtype=np.int32)
 
 
-def find_low_points(hmap: Dict[complex, int]) -> int:
-
-    risk = 0
-    for coord, val in hmap.items():
-        neighbors = []
-
-        for offset in ADJACENT:
-            neighbor = coord + offset
-            if neighbor in hmap:
-                neighbors.append(hmap.get(neighbor))
+def find_low_points(hmap: np.array) -> int:
+    def _kernel(arr: np.array) -> int:
+        neighbors = arr[1::2]
+        val = arr[4]
         is_low = all([val < n for n in neighbors])
+        return 1 if is_low else 0
 
-        if is_low:
-            risk += 1 + val
+    minima = generic_filter(hmap, _kernel, size=3, mode="constant", cval=np.inf)
+    risk = np.sum(hmap[minima == 1] + 1)
 
     return risk
+
+
+def find_basins(hmap: np.array) -> int:
+
+    hmap += 1
+    hmap[hmap == 10] = 0
+
+    clusters, _ = label(hmap)
+    counts = np.bincount(clusters.flatten())
+    largest = np.prod(sorted(counts[1:])[-3:])
+
+    return largest
 
 
 if __name__ == "__main__":
@@ -41,3 +44,6 @@ if __name__ == "__main__":
 
     part1 = find_low_points(hmap)
     print(part1)
+
+    part2 = find_basins(hmap)
+    print(part2)
