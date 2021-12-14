163a76569b63286566159181d2ccfb4b65cfe434
xadrianzetx <xadrianzetx@gmail.com>
2021-12-14 11:05:50 +0100

    Small refactor

--- 2021/day14/sol.py
+++ 2021/day14/sol.py
@@ -18,21 +18,18 @@ def grow_polymer(sequence: str, template: Dict[str, str], iter: int) -> int:
     pairs, counts = defaultdict(int), defaultdict(int)
     for left, right in zip(sequence[:-1], sequence[1:]):
         pairs[f"{left}{right}"] += 1
-
-    for char in sequence:
-        counts[char] += 1
+        counts[left] += 1
+    counts[right] += 1
 
     for _ in range(iter):
         pcopy = pairs.copy()
-        for key, val in template.items():
-            count = pcopy[key]
-            pairs[key] -= count
-            counts[val] += count
-            left, right = list(key)
-            newleft = f"{left}{val}"
-            newright = f"{val}{right}"
-            pairs[newleft] += count
-            pairs[newright] += count
+        for pair, insert in template.items():
+            count = pcopy[pair]
+            pairs[pair] -= count
+            counts[insert] += count
+            left, right = list(pair)
+            pairs[f"{left}{insert}"] += count
+            pairs[f"{insert}{right}"] += count
 
     return max(counts.values()) - min(counts.values())
 
