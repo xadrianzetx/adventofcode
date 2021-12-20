e3de8fd093e4656cd46a28e91e4a0ac4ca1e1395
xadrianzetx <xadrianzetx@gmail.com>
2021-12-20 13:43:52 +0100

    Speed up

--- 2021/day20/sol.py
+++ 2021/day20/sol.py
@@ -1,10 +1,11 @@
 from typing import List, Tuple
 
 import numpy as np
+from numba import jit
 from scipy.ndimage.filters import generic_filter
 
 
-def read_image(filename: str) -> Tuple[np.array, List[int]]:
+def read_image(filename: str) -> Tuple[np.array, Tuple[int]]:
 
     with open(filename) as file:
         data = file.read()
@@ -17,25 +18,32 @@ def read_image(filename: str) -> Tuple[np.array, List[int]]:
     img = np.array(img, dtype=np.uint8)
     img = np.pad(img, ((50, 50), (50, 50)), constant_values=0)
 
-    return img, lut
+    return img, tuple(lut)
 
 
-def enhance(img: np.array, lut: List[int]):
-    def _kernel(arr: np.array) -> int:
-        bin_idx = "".join([str(int(x)) for x in arr])
-        return lut[int(bin_idx, 2)]
+def enhance(img: np.array, lut: List[int], n_iter: int) -> List[int]:
+    @jit(nopython=True)
+    def _kernel(arr: np.ndarray) -> int:
+        idx = 0
+        for bit in arr:
+            idx = 2 * idx + int(bit)
+        return lut[idx]
 
-    # Since first and last bits in image enhancement algorithm
-    # are respectively 1 and 0, whole grid will switch to light every
-    # other iteration. Edges have to account for this.
-    cval = 1 if img[0, 0] == 1 else 0
-    return generic_filter(img, _kernel, size=3, mode="constant", cval=cval)
+    lit = []
+    for _ in range(n_iter):
+        # Since first and last bits in image enhancement algorithm
+        # are respectively 1 and 0, whole grid will switch to light every
+        # other iteration. Edges have to account for this.
+        cval = 1 if img[0, 0] == 1 else 0
+        img = generic_filter(img, _kernel, size=3, mode="constant", cval=cval)
+        lit.append(img.sum())
+
+    return lit
 
 
 if __name__ == "__main__":
     img, lut = read_image("d20.txt")
-    for i in range(50):
-        img = enhance(img, lut)
-        if i in [1, 49]:
-            # Parts 1 and 2.
-            print(img.sum())
+
+    lit = enhance(img, lut, 50)
+    print(lit[1])  # Part 1
+    print(lit[-1])  # Part 2
