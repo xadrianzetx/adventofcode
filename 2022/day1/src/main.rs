a181b2ab359eb35f8b3a7a3560afd78019606b61
xadrianzetx <xadrianzetx@gmail.com>
2022-12-01 11:08:24 +0100

    Cleanup part 1 and part 2

--- 2022/day1/src/main.rs
+++ 2022/day1/src/main.rs
@@ -1,17 +1,15 @@
 fn main() {
-    let mut count = 0;
-    let mut calorie_vec = Vec::new();
-    include_str!("../in").lines().for_each(|line| {
-        if line.is_empty() {
-            calorie_vec.push(count);
-            count = 0;
-        } else {
-            let calories: i32 = line.parse().unwrap();
-            count += calories;
-        }
+    let mut calories = Vec::new();
+    include_str!("../input").split("\n\n").for_each(|group| {
+        let calorie_count = group
+            .lines()
+            .map(|line| line.parse::<i32>().unwrap())
+            .sum::<i32>();
+        calories.push(calorie_count);
     });
-    calorie_vec.sort_unstable();
-    calorie_vec.reverse();
-    println!("Part1: {}", calorie_vec[0]);
-    println!("Part2: {}", &calorie_vec[0..3].iter().sum::<i32>());
+
+    calories.sort_unstable();
+    calories.reverse();
+    println!("Part1: {}", calories[0]);
+    println!("Part2: {}", &calories[0..3].iter().sum::<i32>());
 }
