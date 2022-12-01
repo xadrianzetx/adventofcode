855153b500e5503d377b5aad145e58310e615186
xadrianzetx <xadrianzetx@gmail.com>
2022-12-01 09:37:36 +0100

    Part 2

--- 2022/day1/src/main.rs
+++ 2022/day1/src/main.rs
@@ -1,16 +1,17 @@
 fn main() {
     let mut count = 0;
-    let mut max = 0;
+    let mut calorie_vec = Vec::new();
     include_str!("../in").lines().for_each(|line| {
-        if line == "" {
+        if line.is_empty() {
+            calorie_vec.push(count);
             count = 0;
         } else {
             let calories: i32 = line.parse().unwrap();
             count += calories;
-            if count > max {
-                max = count;
-            }
         }
     });
-    println!("{}", max);
+    calorie_vec.sort_unstable();
+    calorie_vec.reverse();
+    println!("Part1: {}", calorie_vec[0]);
+    println!("Part2: {}", &calorie_vec[0..3].iter().sum::<i32>());
 }
