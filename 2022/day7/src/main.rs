2256b491673f0afe1630e7a6862b75664133f6fb
xadrianzetx <xadrianzetx@gmail.com>
2022-12-07 11:37:26 +0100

    Cleanup

--- 2022/day7/src/main.rs
+++ 2022/day7/src/main.rs
@@ -1,68 +1,46 @@
 use std::collections::HashMap;
-use std::path::Path;
+use std::path::PathBuf;
 
-#[derive(Debug)]
-struct File {
-    name: String,
-    size: u32,
-}
-
-#[derive(Default, Debug)]
+#[derive(Default)]
 struct Directory {
-    name: String,
-    children: Vec<String>,
-    files: Vec<File>,
+    children: Vec<PathBuf>,
+    files: Vec<u32>,
 }
 
 #[derive(Default)]
 struct FileSystem {
-    curdir: String,
-    fs: HashMap<String, Directory>    
-}
-
-impl From<&str> for Directory {
-    fn from(name: &str) -> Self {
-        Directory { name: String::from(name), ..Default::default() }
-    }
+    current_dir: PathBuf,
+    fs: HashMap<PathBuf, Directory>,
 }
 
 impl Directory {
-    fn get_size(&self, fs: &FileSystem) -> u32 {
-        let mut total = 0;
-        for child in &self.children {
-            total += fs.fs.get(child).unwrap().get_size(fs);
-        }
-        for file in &self.files {
-            total += file.size;
-        }
-        total
+    fn get_size(&self, file_system: &FileSystem) -> u32 {
+        self.children
+            .iter()
+            .map(|c| file_system.fs.get(c).unwrap().get_size(file_system))
+            .sum::<u32>()
+            + self.files.iter().sum::<u32>()
     }
 }
 
 impl FileSystem {
     fn sum_limited_size(&self) -> u32 {
-        let mut total = 0;
-        for (_, dir) in &self.fs {
-            // println!("{:?}", dir);
-            let size = dir.get_size(self);
-            if size <= 100000 {
-                total += size;
-            }
-        }
-        total
+        self.fs
+            .values()
+            .map(|d| d.get_size(self))
+            .filter(|v| v <= &100000)
+            .sum()
     }
 
-    fn find_smallest(&self) -> u32 {
-        let used = 70000000 - self.fs.get("/").unwrap().get_size(self);
+    fn find_smallest_to_delete(&self) -> u32 {
+        let used = 70000000 - self.fs.get(&PathBuf::from("/")).unwrap().get_size(self);
         let required = 30000000 - used;
-        let mut min = 70000000;
-        for (_, dir) in &self.fs {
-            let size = dir.get_size(self);
-            if size >= required && size < min {
-                min = size;
-            }
-        }
-        min
+        self.fs
+            .values()
+            .map(|d| d.get_size(self))
+            .filter(|v| v >= &required)
+            .min()
+            .unwrap()
     }
 }
 
@@ -71,29 +49,19 @@ impl FileSystem {
         let parts: Vec<&str> = command.split(' ').collect();
         match parts[1] {
             "cd" => {
-                let newpath = match parts[2] {
-                    "/" => {
-                        String::from("/")
-                    },
-                    ".." => {
-                        let pth = Path::new(&self.curdir);
-                        String::from(pth.parent().unwrap().to_str().unwrap())
-                    },
-                    _ => {
-                        let p = Path::new(&self.curdir);
-                        String::from(p.join(parts[2]).as_path().to_str().unwrap())
-                    }
+                let next_dir = match parts[2] {
+                    "/" => PathBuf::from("/"),
+                    ".." => self.current_dir.parent().unwrap().to_path_buf(),
+                    _ => PathBuf::from(&self.current_dir).join(parts[2]),
                 };
-                
-                if !self.fs.contains_key(&newpath) {
-                    let newdir = Directory::from(newpath.as_str());
-                    self.fs.insert(newpath.clone(), newdir);
+
+                if !self.fs.contains_key(&next_dir) {
+                    self.fs.insert(next_dir.clone(), Directory::default());
                 }
-                self.curdir = newpath.clone();
-                // println!("{}", self.curdir);
-            },
+                self.current_dir = next_dir;
+            }
             "ls" => (),
-            _ => panic!()
+            _ => panic!(),
         }
     }
 
@@ -101,15 +69,13 @@ impl FileSystem {
         let parts: Vec<&str> = data.split(' ').collect();
         match parts[0] {
             "dir" => {
-                let p = Path::new(&self.curdir);
-                let child = String::from(p.join(parts[1]).as_path().to_str().unwrap());
-                let dir = self.fs.get_mut(&self.curdir).unwrap();
-                dir.children.push(child);
-            },
+                let dir = self.fs.get_mut(&self.current_dir).unwrap();
+                let path = PathBuf::from(&self.current_dir).join(parts[1]);
+                dir.children.push(path);
+            }
             _ => {
-                let file = File {name: String::from(parts[1]), size: parts[0].parse().unwrap()};
-                let dir = self.fs.get_mut(&self.curdir).unwrap();
-                dir.files.push(file);
+                let dir = self.fs.get_mut(&self.current_dir).unwrap();
+                dir.files.push(parts[0].parse().unwrap());
             }
         }
     }
@@ -117,13 +83,13 @@ impl FileSystem {
 
 fn main() {
     let mut fs = FileSystem::default();
-    include_str!("../test-input").lines().for_each(|line| {
-        if line.starts_with("$") {
+    include_str!("../input").lines().for_each(|line| {
+        if line.starts_with('$') {
             fs.run_command(line);
         } else {
             fs.register_data(line);
         }
     });
-    println!("{}", fs.sum_limited_size());
-    println!("{}", fs.find_smallest());
+    println!("Part1: {}", fs.sum_limited_size());
+    println!("Part2: {}", fs.find_smallest_to_delete());
 }
