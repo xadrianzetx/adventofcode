e80933617d07ca5001991619a752c79ad0c6998e
xadrianzetx <xadrianzetx@gmail.com>
2022-12-08 12:05:29 +0100

    Cleanup?

--- 2022/day8/src/main.rs
+++ 2022/day8/src/main.rs
@@ -1,3 +1,4 @@
+use std::cmp::max;
 use std::collections::HashMap;
 
 fn main() {
@@ -7,75 +8,54 @@ fn main() {
     include_str!("../input").lines().for_each(|line| {
         colcount = line.chars().count();
         line.chars().enumerate().for_each(|c| {
-            trees.insert((rowcount, c.0), c.1.to_digit(10).unwrap() as i32);
+            trees.insert((rowcount, c.0), c.1.to_digit(10).unwrap());
         });
         rowcount += 1;
     });
 
-    let mut totals = 0;
-    for (loc, tree) in &trees {
-        let mut ca = 0;
-        let mut cm = false;
-        (0..loc.0)
-            .into_iter()
-            .map(|r| trees.get(&(r, loc.1)).unwrap())
-            .rev()
-            .for_each(|t| {
-                if !cm {
-                    ca += 1;
-                }
-                if t >= tree {
-                    cm = true
-                }
-            });
-        
-        let mut cb = 0;
-        cm = false;
-        (loc.0 + 1..rowcount)
-            .into_iter()
-            .map(|r| trees.get(&(r, loc.1)).unwrap())
-            .for_each(|t| {
-                if !cm {
-                    cb += 1;
-                }
-                if t >= tree {
-                    cm = true;
-                }
-            });
-        
-        let mut cc = 0;
-        cm = false;
-        (0..loc.1)
-            .into_iter()
-            .map(|r| trees.get(&(loc.0, r)).unwrap())
-            .rev()
-            .for_each(|t| {
-                if !cm {
-                    cc += 1;
-                }
-                if t >= tree {
-                    cm = true;
-                }
-            });
-        
-        let mut cd = 0;
-        cm = false;
-        (loc.1 + 1..colcount)
-            .into_iter()
-            .map(|r| trees.get(&(loc.0, r)).unwrap())
-            .for_each(|t| {
-                if !cm {
-                    cd += 1;
-                }
-                if t >= tree {
-                    cm = true;
-                }
-            });
-        
-        let dist = ca * cb * cc * cd;
-        if dist > totals {
-            totals = dist;
-        }
-    }
-    println!("{}", totals);
+    let ans = trees
+        .iter()
+        .map(|t| {
+            let coord = t.0;
+            let tree = t.1;
+            let ans = vec![
+                (0..coord.0)
+                    .into_iter()
+                    .map(|c| trees.get(&(c, coord.1)).unwrap().to_owned())
+                    .rev()
+                    .collect::<Vec<u32>>(),
+                (coord.0 + 1..rowcount)
+                    .map(|c| trees.get(&(c, coord.1)).unwrap().to_owned())
+                    .collect(),
+                (0..coord.1)
+                    .map(|c| trees.get(&(coord.0, c)).unwrap().to_owned())
+                    .rev()
+                    .collect(),
+                (coord.1 + 1..colcount)
+                    .map(|c| trees.get(&(coord.0, c)).unwrap().to_owned())
+                    .collect(),
+            ]
+            .iter()
+            .map(|neighbors| {
+                let mut visible = true;
+                let mut score: usize = 0;
+                neighbors.iter().for_each(|t| {
+                    if visible {
+                        score += 1;
+                    }
+                    if t >= tree {
+                        visible = false;
+                    }
+                });
+                (visible, score)
+            })
+            .reduce(|a, b| (a.0 | b.0, a.1 * b.1))
+            .unwrap();
+            (ans.0 as usize, ans.1)
+        })
+        .reduce(|a, b| (a.0 + b.0, max(a.1, b.1)))
+        .unwrap();
+
+    println!("Part1: {}", ans.0);
+    println!("Part2: {}", ans.1);
 }
