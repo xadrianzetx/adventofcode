a5d4a14443b59e511176d08693690386ca48a5a7
xadrianzetx <xadrianzetx@gmail.com>
2022-12-09 18:06:05 +0100

    More cleanup

--- 2022/day9/src/main.rs
+++ 2022/day9/src/main.rs
@@ -1,7 +1,5 @@
+use std::cmp::{max, min};
 use std::collections::HashSet;
-use std::cmp::{min, max};
-
-type Rope = (i32, i32);
 
 enum Directions {
     Up,
@@ -17,50 +15,126 @@ impl From<&str> for Directions {
             "D" => Directions::Down,
             "L" => Directions::Left,
             "R" => Directions::Right,
-            _ => panic!()
+            _ => panic!(),
         }
     }
 }
 
-fn move_head(head: &mut Rope, direction: &Directions) {
-    match direction {
-        Directions::Up => head.0 += 1,
-        Directions::Down => head.0 -= 1,
-        Directions::Left => head.1 -= 1,
-        Directions::Right => head.1 += 1,
-    };
+#[derive(PartialEq, Eq, Hash, Clone, Copy)]
+struct Position {
+    x: i32,
+    y: i32,
 }
 
-fn move_rope(rope: &mut Vec<Rope>, pos: usize) {
-    let head = rope[pos - 1];
-    let dx = head.0 - rope[pos].0;
-    let dy = head.1 - rope[pos].1;
-    if (dx.abs() > 1 && dy != 0) || (dy.abs() > 1 && dx != 0) {
-        rope[pos].0 += min(max(dx, -1), 1);
-        rope[pos].1 += min(max(dy, -1), 1);
-    } else {
-        rope[pos].0 += dx / 2;
-        rope[pos].1 += dy / 2;
+impl Position {
+    fn new(x: i32, y: i32) -> Position {
+        Position { x, y }
+    }
+}
+
+struct Knot {
+    position: Position,
+    visited: HashSet<Position>,
+    next: Option<Box<Knot>>,
+}
+
+impl Knot {
+    fn new(next: Option<Box<Knot>>) -> Knot {
+        let position = Position::new(0, 0);
+        let visited = HashSet::new();
+        Knot {
+            position,
+            visited,
+            next,
+        }
+    }
+
+    fn update_head_position(&mut self, direction: &Directions) {
+        match direction {
+            Directions::Left => self.position.x -= 1,
+            Directions::Right => self.position.x += 1,
+            Directions::Up => self.position.y += 1,
+            Directions::Down => self.position.y -= 1,
+        };
+
+        self.visited.insert(self.position);
+        if let Some(next) = &mut self.next {
+            next.cascade_updates(&self.position);
+        }
+    }
+
+    fn cascade_updates(&mut self, prev: &Position) {
+        let dx = prev.x - self.position.x;
+        let dy = prev.y - self.position.y;
+        if (dx.abs() > 1 && dy != 0) || (dy.abs() > 1 && dx != 0) {
+            self.position.x += min(max(dx, -1), 1);
+            self.position.y += min(max(dy, -1), 1);
+        } else {
+            self.position.x += dx / 2;
+            self.position.y += dy / 2;
+        }
+
+        self.visited.insert(self.position);
+        if let Some(next) = &mut self.next {
+            next.cascade_updates(&self.position);
+        }
+    }
+
+    fn count_visited_by_tail(&self) -> usize {
+        if let Some(next) = &self.next {
+            return next.count_visited_by_tail();
+        }
+        self.visited.len()
+    }
+
+    fn count_visited_by(&self, n: usize) -> usize {
+        if let Some(next) = &self.next {
+            if n > 0 {
+                return next.count_visited_by(n - 1);
+            }
+        }
+        self.visited.len()
+    }
+}
+
+struct Rope {
+    head: Knot,
+}
+
+impl Rope {
+    fn with_length(length: usize) -> Rope {
+        let mut knot = None;
+        for _ in 0..length {
+            knot = Some(Box::new(Knot::new(knot)));
+        }
+        Rope {
+            head: *knot.unwrap(),
+        }
+    }
+
+    fn update_position(&mut self, direction: &Directions) {
+        self.head.update_head_position(direction);
+    }
+
+    fn count_visited_by_tail(&self) -> usize {
+        self.head.count_visited_by_tail()
+    }
+
+    fn count_visited_by(&self, n: usize) -> usize {
+        self.head.count_visited_by(n)
     }
 }
 
 fn main() {
-    let mut rope: Vec<Rope> = vec![(0, 0); 10];
-    let mut visited = HashSet::new();
-    let mut visited2 = HashSet::new();
+    let mut rope = Rope::with_length(10);
     include_str!("../input").lines().for_each(|line| {
         let mut d = line.split(' ');
         let direction = Directions::from(d.next().unwrap());
         let amt: i32 = d.next().unwrap().parse().unwrap();
         for _ in 0..amt {
-            move_head(&mut rope[0], &direction);
-            for i in 1..rope.len() {
-                move_rope(&mut rope, i);
-            }
-            visited.insert(rope[1].clone());
-            visited2.insert(rope.last().copied().unwrap());
+            rope.update_position(&direction);
         }
     });
-    println!("Part1: {}", visited.len());
-    println!("Part2: {}", visited2.len());
+    println!("Part1: {}", rope.count_visited_by(1));
+    println!("Part2: {}", rope.count_visited_by_tail());
 }
