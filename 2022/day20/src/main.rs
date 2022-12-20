16e4e31871647c1bb41bbea6e31a80166862409c
xadrianzetx <xadrianzetx@gmail.com>
2022-12-20 11:42:28 +0100

    Part 1 and part 2

---
+++ 2022/day20/src/main.rs
@@ -0,0 +1,42 @@
+#[derive(Clone)]
+struct Value {
+    index: usize,
+    value: i64,
+}
+
+impl Value {
+    fn with_original_index(index: usize, value: i64) -> Self {
+        Value { index, value }
+    }
+}
+
+fn prepare_for_mixing(data: &str, key: i64) -> Vec<Value> {
+    data.lines()
+        .enumerate()
+        .map(|(index, line)| Value::with_original_index(index, line.parse::<i64>().unwrap() * key))
+        .collect::<Vec<Value>>()
+}
+
+fn mix(values: Vec<Value>, n_iter: usize) -> i64 {
+    let mut target = values.clone();
+    for _ in 0..n_iter {
+        for offset in values.iter() {
+            let idx = target.iter().position(|v| v.index == offset.index).unwrap();
+            let val = target.remove(idx);
+            let tl = target.len() as i64;
+            let newidx = ((idx as i64 + offset.value % tl) + tl) % tl;
+            target.insert(newidx as usize, val);
+        }
+    }
+
+    let zeropos = target.iter().position(|v| v.value == 0).unwrap();
+    target[(zeropos + 1000) % target.len()].value
+        + target[(zeropos + 2000) % target.len()].value
+        + target[(zeropos + 3000) % target.len()].value
+}
+
+fn main() {
+    let data = include_str!("../input");
+    println!("Part1: {}", mix(prepare_for_mixing(data, 1), 1));
+    println!("Part1: {}", mix(prepare_for_mixing(data, 811589153), 10));
+}
