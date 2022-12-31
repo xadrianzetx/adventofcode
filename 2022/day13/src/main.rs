c2ea4bf96d24a33fddae45a53fec2d5b610a9ff1
xadrianzetx <xadrianzetx@gmail.com>
2022-12-31 16:02:01 +0100

    Cleanup

--- 2022/day13/src/main.rs
+++ 2022/day13/src/main.rs
@@ -1,60 +1,42 @@
-use itertools::EitherOrBoth::{Both, Left, Right};
+use itertools::EitherOrBoth::*;
 use itertools::Itertools;
 use serde::Deserialize;
 use std::cmp::Ordering;
 
-#[derive(PartialEq, Eq)]
-enum PacketOrdering {
-    Ok,
-    Wrong,
-    Undefined,
-}
-
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, PartialEq, Eq)]
 #[serde(untagged)]
 enum Packet {
     Val(u8),
     Arr(Vec<Packet>),
 }
 
-fn compare(left: &Packet, right: &Packet) -> PacketOrdering {
-    // TODO(xadrianzetx) Impl Ord
-    // https://github.com/timvisee/advent-of-code-2022/blob/master/day13b/src/main.rs
-    use Packet::*;
-    match (left, right) {
-        (Arr(l), Arr(r)) => {
-            for lr in l.iter().zip_longest(r.iter()) {
-                let ord = match lr {
-                    Left(_) => PacketOrdering::Wrong,
-                    Right(_) => PacketOrdering::Ok,
-                    Both(a, b) => compare(a, b),
-                };
-                if ord != PacketOrdering::Undefined {
-                    return ord;
+impl Ord for Packet {
+    fn cmp(&self, other: &Self) -> Ordering {
+        use Packet::*;
+        match (self, other) {
+            (Val(left), Val(right)) => left.cmp(right),
+            (Val(left), Arr(_)) => Arr(vec![Val(*left)]).cmp(other),
+            (Arr(_), Val(right)) => self.cmp(&Arr(vec![Val(*right)])),
+            (Arr(left), Arr(right)) => {
+                for lr in left.iter().zip_longest(right.iter()) {
+                    let ord = match lr {
+                        Left(_) => Ordering::Greater,
+                        Right(_) => Ordering::Less,
+                        Both(a, b) => a.cmp(b),
+                    };
+                    if !ord.is_eq() {
+                        return ord;
+                    }
                 }
+                Ordering::Equal
             }
-            PacketOrdering::Undefined
         }
-        (Val(l), Val(r)) => match l.cmp(r) {
-            Ordering::Less => PacketOrdering::Ok,
-            Ordering::Greater => PacketOrdering::Wrong,
-            Ordering::Equal => PacketOrdering::Undefined,
-        },
-        (Val(l), Arr(_)) => compare(&Arr(vec![Val(*l)]), right),
-        (Arr(_), Val(r)) => compare(left, &Arr(vec![Val(*r)])),
     }
 }
 
-fn is_divider_packet(packets: &Packet) -> bool {
-    use Packet::*;
-    match packets {
-        &Val(n) => [2, 6].contains(&n),
-        Arr(a) => {
-            if a.is_empty() || a.len() > 1 {
-                return false;
-            }
-            is_divider_packet(&a[0])
-        }
+impl PartialOrd for Packet {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
     }
 }
 
@@ -62,55 +44,42 @@ fn part1(data: &str) {
     let ans = data
         .split("\n\n")
         .enumerate()
-        .filter_map(|(index, pair)| {
-            let lr = pair
+        .filter(|(_, pair)| {
+            let (left, right) = pair
                 .split('\n')
                 .map(|row| serde_json::from_str::<Packet>(row).unwrap())
-                .collect::<Vec<Packet>>();
-
-            match compare(&lr[0], &lr[1]) {
-                PacketOrdering::Ok => Some(index + 1),
-                _ => None,
-            }
+                .collect_tuple::<(Packet, Packet)>()
+                .unwrap();
+            left < right
         })
+        .map(|(index, _)| index + 1)
         .sum::<usize>();
     println!("Part1: {}", ans);
 }
 
 fn part2(data: &str) {
+    use Packet::*;
     let mut packets: Vec<Packet> = Vec::new();
     data.lines().filter(|l| !l.is_empty()).for_each(|row| {
         let packet = serde_json::from_str::<Packet>(row).unwrap();
         packets.push(packet);
     });
+
     packets.push(serde_json::from_str::<Packet>("[[2]]").unwrap());
     packets.push(serde_json::from_str::<Packet>("[[6]]").unwrap());
+    packets.sort_unstable();
 
-    // TODO(xadrianzetx) Implementing Ord will allow to just call packets.sort_unstable() :^)
-    loop {
-        let mut swapped = false;
-        for i in 0..packets.len() - 1 {
-            if let PacketOrdering::Wrong = compare(&packets[i], &packets[i + 1]) {
-                packets.swap(i, i + 1);
-                swapped = true;
-            }
-        }
-        if !swapped {
-            break;
-        }
-    }
-
-    let ans = packets
+    let ans: usize = [2, 6]
         .iter()
-        .enumerate()
-        .filter_map(|(idx, p)| {
-            if is_divider_packet(p) {
-                return Some(idx + 1);
-            };
-            None
+        .map(|divider| {
+            packets
+                .iter()
+                .filter(|p| *p < &Arr(vec![Arr(vec![Val(*divider)])]))
+                .count()
+                + 1
         })
-        .collect::<Vec<usize>>();
-    println!("Part2: {}", ans[0] * ans[1]);
+        .product();
+    println!("Part2: {}", ans);
 }
 
 fn main() {
