bdde0ef65dc5aff744f7ba282740c7a02d403601
xadrianzetx <xadrianzetx@gmail.com>
2023-12-31 19:05:56 +0100

    Use shoelace formula and Pick's theorem to measure the lagoon

--- 2023/day18/src/main.rs
+++ 2023/day18/src/main.rs
@@ -1,7 +1,7 @@
-use std::collections::HashSet;
+use geo::{Area, LineString, Polygon};
 
 // Up, right, down, left.
-const DIRECTIONS: &[(i64, i64)] = &[(-1, 0), (0, 1), (1, 0), (0, -1)];
+const DIRECTIONS: &[(f64, f64)] = &[(-1., 0.), (0., 1.), (1., 0.), (0., -1.)];
 
 fn get_directions(raw_directions: &str) -> usize {
     match raw_directions {
@@ -17,150 +17,64 @@ fn rotate_directions(raw_directions: usize) -> usize {
     (raw_directions + 1) % 4
 }
 
-fn measure_lagoon(vertices: Vec<(i64, i64)>) -> i64 {
-    let mut rows = vertices
-        .iter()
-        .map(|e| e.0)
-        .collect::<HashSet<i64>>()
-        .into_iter()
-        .collect::<Vec<i64>>();
-    rows.sort();
-
-    let mut nextrows = rows.clone();
-    nextrows.push(rows.last().unwrap() - 1);
-
-    let mut ranges: Vec<(i64, i64)> = Vec::new();
-    let mut cnt = 0;
-
-    for (row, nextrow) in rows.iter().zip(nextrows.iter().skip(1)) {
-        let mut r = vertices
-            .iter()
-            .filter(|e| e.0 == *row)
-            .collect::<Vec<&(i64, i64)>>();
-        r.sort_by_key(|k| k.1);
-
-        for pair in r.chunks(2) {
-            let newrng = (pair[0].1, pair[1].1);
-            let mut toadd = true;
-            let mut splitat = None;
-            let mut splitrng = None;
-            let mut ending = None;
-            for (idx, range) in ranges.iter_mut().enumerate() {
-                if range == &newrng {
-                    // Range ends
-                    if ranges.len() > 1 {
-                        ending = Some(idx);
-                    }
-                    toadd = false;
-                    break;
-                }
-
-                if newrng.0 == range.1 {
-                    // Extending right.
-                    *range = (range.0, newrng.1);
-                    toadd = false;
-                } else if newrng.1 == range.0 {
-                    // Extending left.
-                    *range = (newrng.0, range.1);
-                    toadd = false;
-                } else if newrng.0 == range.0 {
-                    // Tighten left
-                    cnt += (newrng.1 - newrng.0).abs();
-                    *range = (newrng.1, range.1);
-                    toadd = false;
-                } else if newrng.1 == range.1 {
-                    // Tighten right
-                    cnt += (newrng.1 - newrng.0).abs();
-                    *range = (range.0, newrng.0);
-                    toadd = false;
-                } else if newrng.0 > range.0 && newrng.1 < range.1 {
-                    // Split
-                    // println!("SPLITS {range:?}, {newrng:?}");
-                    cnt += (newrng.1 - newrng.0).abs();
-                    splitat = Some(idx);
-                    splitrng = Some(vec![(range.0, newrng.0), (newrng.1, range.1)]);
-                    toadd = false;
-                } else {
-                    // Shitcode!
-                }
-            }
-
-            if toadd {
-                ranges.push(newrng);
-            }
-            if let Some(ended) = ending {
-                cnt += (ranges[ended].1 - ranges[ended].0).abs();
-                ranges.remove(ended);
-            }
-            if let Some(splitidx) = splitat {
-                ranges = [
-                    &ranges[..splitidx],
-                    &splitrng.unwrap(),
-                    &ranges[splitidx + 1..],
-                ]
-                .concat();
-            }
-            ranges.sort_by_key(|k| k.0);
-            // Range concatenated
-            let mut i = 0;
-            while i < ranges.len() - 1 {
-                if ranges[i].1 >= ranges[i + 1].0 {
-                    // println!("CONCAT {:?} {:?}", ranges[i], ranges[i + 1]);
-                    let a = ranges.remove(i);
-                    let b = ranges.remove(i);
-                    ranges.push((a.0, b.1));
-                    break;
-                } else {
-                    i += 1;
-                }
-            }
-        }
-
-        let mul = (nextrow - row).abs();
-        cnt += ranges
-            .iter()
-            .map(|rng| ((rng.1 - rng.0).abs() + 1) * mul)
-            .sum::<i64>();
-    }
-
-    cnt
+fn parse_small_plan(raw_plan: &str) -> Vec<(usize, f64)> {
+    raw_plan
+        .lines()
+        .map(|line| {
+            let mut plan = line.split(' ');
+            let direction = get_directions(plan.next().unwrap());
+            let distance = plan.next().unwrap().parse::<f64>().unwrap();
+            (direction, distance)
+        })
+        .collect::<Vec<(usize, f64)>>()
 }
 
-fn small_plan(raw_plan: &str) -> Vec<(i64, i64)> {
+fn parse_big_plan(raw_plan: &str) -> Vec<(usize, f64)> {
+    raw_plan
+        .lines()
+        .map(|line| {
+            let plan = line.split(' ');
+            let mut hex = plan.last().unwrap().replace(['(', ')', '#'], "");
+            let direction = rotate_directions(hex.pop().unwrap().to_digit(10).unwrap() as usize);
+            let distance = i64::from_str_radix(&hex, 16).unwrap() as f64;
+            (direction, distance)
+        })
+        .collect::<Vec<(usize, f64)>>()
+}
+
+fn calculate_lagoon_area(vertices: Vec<(f64, f64)>) -> usize {
+    let polygon = Polygon::new(LineString::from(vertices), vec![]);
+    polygon.unsigned_area() as usize
+}
+
+fn calculate_lagoon_capacity(plan: Vec<(usize, f64)>) -> usize {
     let mut buff = Vec::new();
-    let mut curr = (0, 0);
-    for line in raw_plan.lines() {
-        let mut plan = line.split(' ');
-        let direction = get_directions(plan.next().unwrap());
-        let distance = plan.next().unwrap().parse::<i64>().unwrap();
+    let mut curr = (0., 0.);
+    let mut perimeter = 0;
+
+    for (direction, distance) in plan {
+        perimeter += distance as usize;
         buff.push((curr.0, curr.1));
+
         curr = (
             curr.0 + (DIRECTIONS[direction].0 * distance),
             curr.1 + (DIRECTIONS[direction].1 * distance),
         );
     }
-    buff
-}
 
-fn big_plan(raw_plan: &str) -> Vec<(i64, i64)> {
-    let mut buff = Vec::new();
-    let mut curr = (0, 0);
-    for line in raw_plan.lines() {
-        let plan = line.split(' ');
-        let mut hex = plan.last().unwrap().replace(['(', ')', '#'], "");
-        let direction = rotate_directions(hex.pop().unwrap().to_digit(10).unwrap() as usize);
-        let distance = i64::from_str_radix(&hex, 16).unwrap();
-        buff.push((curr.0, curr.1));
-        curr = (
-            curr.0 + (DIRECTIONS[direction].0 * distance),
-            curr.1 + (DIRECTIONS[direction].1 * distance),
-        );
-    }
-    buff
+    calculate_lagoon_area(buff) + (perimeter / 2) + 1
 }
 
 fn main() {
     let raw_plan = include_str!("../input");
-    println!("Part 1: {}", measure_lagoon(small_plan(raw_plan)));
-    println!("Part 2: {}", measure_lagoon(big_plan(raw_plan)));
+
+    println!(
+        "Part 1: {}",
+        calculate_lagoon_capacity(parse_small_plan(raw_plan))
+    );
+
+    println!(
+        "Part 2: {}",
+        calculate_lagoon_capacity(parse_big_plan(raw_plan))
+    );
 }
