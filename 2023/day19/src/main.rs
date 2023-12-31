b48e4d439764776cfa90ac08eaa0003f72dbae83
xadrianzetx <xadrianzetx@gmail.com>
2023-12-31 17:08:23 +0100

    Switch to a tree representation of workflows

--- 2023/day19/src/main.rs
+++ 2023/day19/src/main.rs
@@ -1,41 +1,10 @@
-use std::collections::{HashMap, VecDeque};
+use std::collections::HashMap;
 
-type Workflows = HashMap<String, Workflow>;
-
-#[derive(Debug, Clone, PartialEq, Eq)]
-enum PartStatus {
-    Accepted,
-    Rejected,
-    Inspect(String),
+trait Rated {
+    fn rate(&self, part: &Part) -> Status;
+    fn count_accepted(&self, part_range: PartRange) -> usize;
 }
 
-impl From<&str> for PartStatus {
-    fn from(value: &str) -> Self {
-        match value {
-            "A" => Self::Accepted,
-            "R" => Self::Rejected,
-            _ => Self::Inspect(value.to_string()),
-        }
-    }
-}
-
-#[derive(Debug)]
-enum Order {
-    Greater,
-    Less,
-}
-
-impl From<char> for Order {
-    fn from(value: char) -> Self {
-        match value {
-            '>' => Self::Greater,
-            '<' => Self::Less,
-            _ => unreachable!(),
-        }
-    }
-}
-
-#[derive(Debug)]
 enum Category {
     X,
     M,
@@ -55,289 +24,235 @@ impl From<char> for Category {
     }
 }
 
-#[derive(Debug)]
-struct Rule {
-    category: Category,
-    order: Order,
-    value: usize,
-    outcome: PartStatus,
-}
-
-impl From<&str> for Rule {
-    fn from(value: &str) -> Self {
-        let mut s = value.split(':');
-        let mut cmp = s.next().unwrap().chars();
-
-        let category = Category::from(cmp.next().unwrap());
-        let order = Order::from(cmp.next().unwrap());
-        let num = cmp.collect::<String>().parse::<usize>().unwrap();
-        let outcome = PartStatus::from(s.next().unwrap());
-
-        Self {
-            category,
-            order,
-            value: num,
-            outcome,
+impl From<&Category> for usize {
+    fn from(value: &Category) -> Self {
+        match value {
+            Category::X => 0,
+            Category::M => 1,
+            Category::A => 2,
+            Category::S => 3,
         }
     }
+}
+
+enum Condition {
+    Greater,
+    Less,
+}
+
+impl From<char> for Condition {
+    fn from(value: char) -> Self {
+        match value {
+            '>' => Self::Greater,
+            '<' => Self::Less,
+            _ => unreachable!(),
+        }
+    }
+}
+
+#[derive(Clone, Copy)]
+enum Status {
+    Accepted,
+    Rejected,
+}
+
+impl Rated for Status {
+    fn rate(&self, _: &Part) -> Status {
+        *self
+    }
+
+    fn count_accepted(&self, part_range: PartRange) -> usize {
+        match self {
+            Self::Rejected => 0,
+            Self::Accepted => part_range.0.iter().map(|rng| rng.1 - rng.0 + 1).product(),
+        }
+    }
+}
+
+impl Status {
+    fn is_accepted(&self) -> bool {
+        match self {
+            Self::Accepted => true,
+            Self::Rejected => false,
+        }
+    }
+}
+
+struct Rule {
+    category: Category,
+    condition: Condition,
+    value: usize,
+    on_pass: Box<dyn Rated>,
+    on_fail: Box<dyn Rated>,
 }
 
 impl Rule {
-    fn matched(&self, part: &mut Part) -> bool {
-        match self.category {
-            Category::X => match self.order {
-                Order::Greater => part.x > self.value,
-                Order::Less => part.x < self.value,
-            },
-            Category::M => match self.order {
-                Order::Greater => part.m > self.value,
-                Order::Less => part.m < self.value,
-            },
-            Category::A => match self.order {
-                Order::Greater => part.a > self.value,
-                Order::Less => part.a < self.value,
-            },
-            Category::S => match self.order {
-                Order::Greater => part.s > self.value,
-                Order::Less => part.s < self.value,
-            },
+    fn new_with_outcomes(raw_rule: &str, on_pass: Box<dyn Rated>, on_fail: Box<dyn Rated>) -> Self {
+        let mut chars = raw_rule.chars();
+        let category = Category::from(chars.next().unwrap());
+        let condition = Condition::from(chars.next().unwrap());
+        let value = chars.collect::<String>().parse::<usize>().unwrap();
+
+        Self {
+            category,
+            condition,
+            value,
+            on_pass,
+            on_fail,
         }
     }
+}
 
-    fn probe(&self, probe: &mut Probe) -> Probe {
-        let mut new_probe = probe.clone();
-        match self.category {
-            Category::X => match self.order {
-                Order::Greater => {
-                    new_probe.x = (self.value + 1, probe.x.1);
-                    probe.x = (probe.x.0, self.value);
-                }
-                Order::Less => {
-                    new_probe.x = (probe.x.0, self.value - 1);
-                    probe.x = (self.value, probe.x.1);
-                }
-            },
-            Category::M => match self.order {
-                Order::Greater => {
-                    new_probe.m = (self.value + 1, probe.m.1);
-                    probe.m = (probe.m.0, self.value);
-                }
-                Order::Less => {
-                    new_probe.m = (probe.m.0, self.value - 1);
-                    probe.m = (self.value, probe.m.1);
-                }
-            },
-            Category::A => match self.order {
-                Order::Greater => {
-                    new_probe.a = (self.value + 1, probe.a.1);
-                    probe.a = (probe.a.0, self.value);
-                }
-                Order::Less => {
-                    new_probe.a = (probe.a.0, self.value - 1);
-                    probe.a = (self.value, probe.a.1);
-                }
-            },
-            Category::S => match self.order {
-                Order::Greater => {
-                    new_probe.s = (self.value + 1, probe.s.1);
-                    probe.s = (probe.s.0, self.value);
-                }
-                Order::Less => {
-                    new_probe.s = (probe.s.0, self.value - 1);
-                    probe.s = (self.value, probe.s.1);
-                }
-            },
+impl Rated for Rule {
+    fn rate(&self, part: &Part) -> Status {
+        let passes = match self.condition {
+            Condition::Greater => part.get_value(&self.category) > self.value,
+            Condition::Less => part.get_value(&self.category) < self.value,
         };
 
-        new_probe.status = self.outcome.clone();
-        new_probe
-    }
-}
-
-#[derive(Debug)]
-struct Workflow {
-    rules: Vec<Rule>,
-    ends_with: PartStatus,
-}
-
-impl Workflow {
-    fn new(raw_rules: &str) -> Self {
-        let mut r = raw_rules.split(',').collect::<Vec<&str>>();
-        let ends_with = PartStatus::from(r.pop().unwrap());
-
-        let mut rules = Vec::new();
-        for raw_rule in r {
-            rules.push(Rule::from(raw_rule));
+        if passes {
+            return self.on_pass.rate(part);
         }
-
-        Self { rules, ends_with }
+        self.on_fail.rate(part)
     }
 
-    fn inspect(&self, part: &mut Part) {
-        for rule in &self.rules {
-            if rule.matched(part) {
-                part.status = rule.outcome.clone();
-                return;
+    fn count_accepted(&self, part_range: PartRange) -> usize {
+        let mut passing = part_range.clone();
+        let mut failing = part_range.clone();
+        let rng = part_range.get_range(&self.category);
+
+        match self.condition {
+            Condition::Greater => {
+                passing.set_range(self.value + 1, rng.1, &self.category);
+                failing.set_range(rng.0, self.value, &self.category);
+            }
+            Condition::Less => {
+                passing.set_range(rng.0, self.value - 1, &self.category);
+                failing.set_range(self.value, rng.1, &self.category);
+            }
+        };
+
+        self.on_pass.count_accepted(passing) + self.on_fail.count_accepted(failing)
+    }
+}
+
+fn build_workflows_lookup(raw_workflows: &str) -> HashMap<String, String> {
+    let mut workflows_lut = HashMap::new();
+    for line in raw_workflows.lines() {
+        let workflow_and_rules = line.split('{').collect::<Vec<&str>>();
+
+        let workflow_name = workflow_and_rules[0];
+        let raw_rules = workflow_and_rules[1];
+
+        for (idx, rule) in raw_rules[..raw_rules.len() - 1].split(',').enumerate() {
+            let flat_worfklow_name = format!("{workflow_name}{idx}");
+            if rule.contains('>') || rule.contains('<') {
+                let next = idx + 1;
+                if rule.ends_with('A') || rule.ends_with('R') {
+                    workflows_lut
+                        .insert(flat_worfklow_name, format!("{rule}:{workflow_name}{next}"));
+                } else {
+                    workflows_lut
+                        .insert(flat_worfklow_name, format!("{rule}0:{workflow_name}{next}"));
+                }
+            } else if rule.ends_with('A') || rule.ends_with('R') {
+                workflows_lut.insert(flat_worfklow_name, rule.to_string());
+            } else {
+                workflows_lut.insert(flat_worfklow_name, format!("{rule}0"));
             }
         }
-
-        part.status = self.ends_with.clone();
     }
 
-    fn probe(&self, mut probe: Probe) -> Vec<Probe> {
-        let mut new_probes = Vec::new();
-        for rule in &self.rules {
-            new_probes.push(rule.probe(&mut probe));
+    workflows_lut
+}
+
+fn add_rule(name: &str, workflows_lut: &HashMap<String, String>) -> Box<dyn Rated> {
+    match name {
+        "A" => Box::new(Status::Accepted),
+        "R" => Box::new(Status::Rejected),
+        _ => {
+            let entry = workflows_lut.get(name).unwrap();
+            let parts = entry.split(':').collect::<Vec<&str>>();
+
+            if parts.len() == 1 {
+                return add_rule(parts[0], workflows_lut);
+            }
+
+            let raw_rule = parts[0];
+            let on_pass = add_rule(parts[1], workflows_lut);
+            let on_fail = add_rule(parts[2], workflows_lut);
+
+            Box::new(Rule::new_with_outcomes(raw_rule, on_pass, on_fail))
         }
-
-        probe.status = self.ends_with.clone();
-        new_probes.push(probe);
-        new_probes
     }
 }
 
-fn build_workflows(raw_workflows: &str) -> Workflows {
-    let mut workflows = HashMap::new();
-    for line in raw_workflows.lines() {
-        let mut s = line.split('{');
-        let name = s.next().unwrap();
-        let raw_rules = s.next().unwrap().replace('}', "");
-        workflows.insert(name.to_string(), Workflow::new(&raw_rules));
-    }
-    workflows
+fn build_workflow_tree(workflows_lut: &HashMap<String, String>) -> Box<dyn Rated> {
+    add_rule("in0", workflows_lut)
 }
 
-#[derive(Debug)]
-struct Part {
-    x: usize,
-    m: usize,
-    a: usize,
-    s: usize,
-    status: PartStatus,
-}
+// The order is x, m, a, s of course!
+struct Part(Vec<usize>);
 
 impl From<&str> for Part {
     fn from(value: &str) -> Self {
-        let strp = value.replace(['{', '}'], "");
-        let mut raw_categories = strp.split(',');
+        let raw_numbers = value.replace(['{', '}', 'x', 'm', 'a', 's', '='], "");
+        let numbers = raw_numbers
+            .split(',')
+            .map(|n| n.parse::<usize>().unwrap())
+            .collect::<Vec<usize>>();
 
-        let x = to_numeric(raw_categories.next().unwrap());
-        let m = to_numeric(raw_categories.next().unwrap());
-        let a = to_numeric(raw_categories.next().unwrap());
-        let s = to_numeric(raw_categories.next().unwrap());
-
-        Self {
-            x,
-            m,
-            a,
-            s,
-            status: PartStatus::Inspect("in".to_string()),
-        }
+        Self(numbers)
     }
 }
 
 impl Part {
-    fn inspect(&mut self, workflows: &Workflows) {
-        while let PartStatus::Inspect(step) = &mut self.status {
-            workflows.get(step).unwrap().inspect(self);
-        }
+    fn sum_rating_numbers(&self) -> usize {
+        self.0.iter().sum()
     }
 
-    fn get_total_rating(&self) -> usize {
-        self.x + self.m + self.a + self.s
-    }
-
-    fn is_accepted(&self) -> bool {
-        self.status == PartStatus::Accepted
+    fn get_value(&self, category: &Category) -> usize {
+        let idx: usize = category.into();
+        *self.0.get(idx).unwrap()
     }
 }
 
-fn to_numeric(value: &str) -> usize {
-    value
-        .matches(char::is_numeric)
-        .collect::<String>()
-        .parse::<usize>()
-        .unwrap()
-}
+#[derive(Clone)]
+struct PartRange(Vec<(usize, usize)>);
 
-fn build_parts(raw_parts: &str) -> Vec<Part> {
-    let mut parts = Vec::new();
-    for line in raw_parts.lines() {
-        parts.push(Part::from(line));
-    }
-    parts
-}
-
-#[derive(Debug, Clone)]
-struct Probe {
-    x: (usize, usize),
-    m: (usize, usize),
-    a: (usize, usize),
-    s: (usize, usize),
-    status: PartStatus,
-}
-
-impl Probe {
-    fn new() -> Self {
-        Self {
-            x: (1, 4000),
-            m: (1, 4000),
-            a: (1, 4000),
-            s: (1, 4000),
-            status: PartStatus::Inspect("in".to_string()),
-        }
+impl PartRange {
+    fn new_with_range(low: usize, high: usize) -> Self {
+        Self(vec![(low, high); 4])
     }
 
-    fn count_combinations(&self) -> usize {
-        (self.x.1 - self.x.0 + 1)
-            * (self.m.1 - self.m.0 + 1)
-            * (self.a.1 - self.a.0 + 1)
-            * (self.s.1 - self.s.0 + 1)
-    }
-}
-
-fn probe_combinations(workflows: &Workflows) -> usize {
-    let mut probed = Vec::new();
-    let initial_probe = Probe::new();
-    let mut to_probe = VecDeque::new();
-    to_probe.push_back(initial_probe);
-
-    while let Some(probe) = to_probe.pop_front() {
-        if let PartStatus::Inspect(workflow) = &probe.status {
-            let new_probes = workflows.get(workflow).unwrap().probe(probe);
-            for new_probe in new_probes {
-                match new_probe.status {
-                    PartStatus::Accepted => probed.push(new_probe),
-                    PartStatus::Rejected => (),
-                    PartStatus::Inspect(_) => to_probe.push_back(new_probe),
-                }
-            }
-        }
+    fn get_range(&self, category: &Category) -> &(usize, usize) {
+        let idx: usize = category.into();
+        self.0.get(idx).unwrap()
     }
 
-    probed.iter().map(|p| p.count_combinations()).sum::<usize>()
+    fn set_range(&mut self, low: usize, high: usize, category: &Category) {
+        let idx: usize = category.into();
+        self.0[idx] = (low, high);
+    }
 }
 
 fn main() {
-    let mut workflows_parts = include_str!("../input").split("\n\n");
-    let raw_workflows = workflows_parts.next().unwrap();
-    let raw_parts = workflows_parts.next().unwrap();
+    let workflows_parts = include_str!("../input")
+        .split("\n\n")
+        .collect::<Vec<&str>>();
 
-    let workflows = build_workflows(raw_workflows);
+    let workflows_lut = build_workflows_lookup(workflows_parts[0]);
+    let workflows = build_workflow_tree(&workflows_lut);
 
-    let mut parts = build_parts(raw_parts);
-    for part in &mut parts {
-        part.inspect(&workflows);
-    }
-
-    let part_1 = parts
-        .iter()
-        .filter(|part| part.is_accepted())
-        .map(|part| part.get_total_rating())
+    let part_1 = workflows_parts[1]
+        .lines()
+        .map(Part::from)
+        .filter(|p| workflows.rate(p).is_accepted())
+        .map(|p| p.sum_rating_numbers())
         .sum::<usize>();
+    println!("{part_1}");
 
-    println!("Part 1: {part_1}");
-
-    let part_2 = probe_combinations(&workflows);
-    println!("Part 2: {part_2}");
+    let part_range = PartRange::new_with_range(1, 4000);
+    let part_2 = workflows.count_accepted(part_range);
+    println!("{part_2}");
 }
